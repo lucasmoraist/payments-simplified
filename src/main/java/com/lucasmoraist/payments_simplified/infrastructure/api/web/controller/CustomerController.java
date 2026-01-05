9bcbb2519bdc00b31e3f350d7cab47ad2fdf523d
lucasmoraist <luksmnt1101@gmail.com>
2026-01-05 10:25:30 -0300

    feat: add getCustomerById endpoint to CustomerController and CustomerRoutes

--- src/main/java/com/lucasmoraist/payments_simplified/infrastructure/api/web/controller/CustomerController.java
+++ src/main/java/com/lucasmoraist/payments_simplified/infrastructure/api/web/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.lucasmoraist.payments_simplified.infrastructure.api.web.controller;
 import com.lucasmoraist.payments_simplified.application.dto.CustomerDTO;
 import com.lucasmoraist.payments_simplified.application.mapper.CustomerMapper;
 import com.lucasmoraist.payments_simplified.application.usecases.customer.CreateCustomerCase;
+import com.lucasmoraist.payments_simplified.application.usecases.customer.FindByCustomerIdCase;
 import com.lucasmoraist.payments_simplified.domain.model.Customer;
 import com.lucasmoraist.payments_simplified.infrastructure.api.web.request.CustomerRequest;
 import com.lucasmoraist.payments_simplified.infrastructure.api.web.router.CustomerRoutes;
@@ -11,12 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class CustomerController implements CustomerRoutes {
 
     private final CreateCustomerCase createCustomerCase;
+    private final FindByCustomerIdCase findByCustomerIdCase;
 
     @Override
     public ResponseEntity<Void> registerCustomer(CustomerRequest request) {
@@ -24,6 +27,12 @@ public class CustomerController implements CustomerRoutes {
         Customer customer = this.createCustomerCase.execute(dto);
         URI uri = URI.create("/api/v1/customer/" + customer.id());
         return ResponseEntity.created(uri).build();
+    }
+
+    @Override
+    public ResponseEntity<Customer> getCustomerById(UUID custId) {
+        Customer customer = this.findByCustomerIdCase.execute(custId);
+        return ResponseEntity.ok().body(customer);
     }
 
 }
--- src/main/java/com/lucasmoraist/payments_simplified/infrastructure/api/web/router/CustomerRoutes.java
+++ src/main/java/com/lucasmoraist/payments_simplified/infrastructure/api/web/router/CustomerRoutes.java
@@ -1,16 +1,24 @@
 package com.lucasmoraist.payments_simplified.infrastructure.api.web.router;
 
+import com.lucasmoraist.payments_simplified.domain.model.Customer;
 import com.lucasmoraist.payments_simplified.infrastructure.api.web.request.CustomerRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @RequestMapping("/api/v1/customer")
 public interface CustomerRoutes {
 
     @PostMapping("/register")
     ResponseEntity<Void> registerCustomer(@RequestBody @Valid CustomerRequest request);
+
+    @GetMapping("{custId}")
+    ResponseEntity<Customer> getCustomerById(@PathVariable UUID custId);
 
 }
