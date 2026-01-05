617d26f5198c7ba33b2de2dc109336b0e18bfc6a
lucasmoraist <luksmnt1101@gmail.com>
2026-01-04 23:42:35 -0300

    feat: add EmailException and NotFoundException for improved error handling

---
+++ src/main/java/com/lucasmoraist/payments_simplified/domain/exceptions/EmailException.java
@@ -0,0 +1,9 @@
+package com.lucasmoraist.payments_simplified.domain.exceptions;
+
+public class EmailException extends RuntimeException {
+
+    public EmailException(String message) {
+        super(message);
+    }
+
+}
