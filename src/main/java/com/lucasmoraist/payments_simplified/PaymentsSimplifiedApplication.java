5b18af2a3a4a472e7ab7aa8ab52d9a74fe3c49d8
lucasmoraist <luksmnt1101@gmail.com>
2026-01-04 23:19:07 -0300

    feat: first commit

---
+++ src/main/java/com/lucasmoraist/payments_simplified/PaymentsSimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.payments_simplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsSimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsSimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoraist/payments_simplified/PaymentsSimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.payments_simplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsSimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
