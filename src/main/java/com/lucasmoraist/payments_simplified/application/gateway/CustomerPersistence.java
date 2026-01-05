eaae3f23b2e8a629f9de98ed240aaad1788cba07
lucasmoraist <luksmnt1101@gmail.com>
2026-01-04 23:45:47 -0300

    feat: implement CustomerPersistence interface and its implementation for customer data handling

---
+++ src/main/java/com/lucasmoraist/payments_simplified/application/gateway/CustomerPersistence.java
@@ -0,0 +1,7 @@
+package com.lucasmoraist.payments_simplified.application.gateway;
+
+import com.lucasmoraist.payments_simplified.domain.model.Customer;
+
+public interface CustomerPersistence {
+    Customer save(Customer customer);
+}
---
+++ src/main/java/com/lucasmoraist/payments_simplified/infrastructure/database/sql/persistence/CustomerPersistenceImpl.java
@@ -0,0 +1,45 @@
+package com.lucasmoraist.payments_simplified.infrastructure.database.sql.persistence;
+
+import com.lucasmoraist.payments_simplified.application.gateway.CustomerPersistence;
+import com.lucasmoraist.payments_simplified.application.mapper.CustomerMapper;
+import com.lucasmoraist.payments_simplified.domain.exceptions.EmailException;
+import com.lucasmoraist.payments_simplified.domain.model.Customer;
+import com.lucasmoraist.payments_simplified.infrastructure.database.sql.entity.CustomerEntity;
+import com.lucasmoraist.payments_simplified.infrastructure.database.sql.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class CustomerPersistenceImpl implements CustomerPersistence {
+
+    private final CustomerRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public Customer save(Customer customer) {
+        log.debug("Saving customer: {}", customer);
+        CustomerEntity entity = CustomerMapper.toEntity(customer);
+        entity.setPassword(this.passwordEncoder.encode(entity.getPassword()));
+
+        this.emailIsExists(customer.email());
+
+        CustomerEntity entitySaved = this.repository.save(entity);
+        log.debug("Customer saved with id: {}", entity.getId());
+        return CustomerMapper.toDomain(entitySaved);
+    }
+
+    private void emailIsExists(String email) {
+        this.repository.findByEmail(email)
+                .ifPresent(c -> {
+                    log.error("Email already exists: {}", c.getEmail());
+                    throw new EmailException("Email already exists");
+                });
+    }
+
+}
